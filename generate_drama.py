3b84af339d839f65fbb41c906e05a04381e542d2
Niki Bizjak <drobilc@gmail.com>
2020-09-12 14:29:32 +0200

    Update drama generator arguments and add (print) logging

--- generate_drama.py
+++ generate_drama.py
@@ -1,27 +1,56 @@
 import argparse
+import os.path
 from drama_generator.data_extractors.facebook import FacebookHTMLDataExtractor
 from drama_generator.generators.latex_generator import LatexGenerator
 from drama_generator.processors import *
 
-parser = argparse.ArgumentParser(description='Make a drama out of your chats')
-parser.add_argument('path', type=str, nargs=1, help='path to the chat directory')
-parser.add_argument('output_file', type=str, nargs=1, help='output file path')
+# Create an argument parser and add arguments to it
+argument_parser = argparse.ArgumentParser(description='Make a drama out of your chats')
+# The only required parameter is INPUT_DIRECTORY, if no output_file argument is
+# passed to the program, a default value is created using input directory name
+argument_parser.add_argument('INPUT_DIRECTORY', type=str, help='path to the chat directory')
+argument_parser.add_argument('-o', '--output-file', dest='output_file', type=str, help='output file path')
 
-args = parser.parse_args()
+# Additional arguments for message processors
+argument_parser.add_argument('--shout', dest='shout', action='store_true', help='write everything using only uppercase letters')
 
-path = args.path[0]
-output_file = args.output_file[0]
+arguments = argument_parser.parse_args()
 
-facebook_html_extractor = FacebookHTMLDataExtractor(path)
+# Parse a list of arguments received
+input_directory = arguments.INPUT_DIRECTORY
+output_file = arguments.output_file
+
+# Build a list of processors that will be applied to a list of messages (use
+# arguments to construct it)
+message_processors = []
+
+if arguments.shout:
+    message_processors.append(UpperCaseMessageProcessor)
+
+# If output file is not provided, generate it using input directory argument
+if output_file is None:
+    output_file_name = os.path.basename(os.path.normpath(input_directory))
+    output_file = os.path.join('generated_dramas', output_file_name)
+
+print('Input directory: {}'.format(input_directory))
+print('Output file: {}'.format(output_file))
+
+print('Parsing messages from input directory')
+# Create a new Facebook HTML extractor and use it to extract messages from
+# received directory
+facebook_html_extractor = FacebookHTMLDataExtractor(input_directory)
 messages = facebook_html_extractor.extract_data()
+print('Messages parsed')
 
-PROCESSORS = [
-    UpperCaseMessageProcessor
-]
-
-for processor in PROCESSORS:
+print('Applying processors to list of messages')
+# Apply each processor to a list of messages
+for processor in message_processors:
     drama_processor = processor()
     messages = drama_processor.process(messages)
+print('Processors applied')
 
+print('Generating output file')
+# Use LaTeX generator to generate drama and save it to output file
 generator = LatexGenerator(messages)
 generator.generate(output_file)
+print('Output file generated')
