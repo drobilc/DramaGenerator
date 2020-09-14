a811a838f84769f0abf4ecf1f33ecaf10c0ade6d
sintakticniSladkorcek <ajda.frankovic@gmail.com>
2020-09-14 23:28:21 +0200

    Add parameters to control text area, font size and indents

--- drama_generator/generators/latex_generator.py
+++ drama_generator/generators/latex_generator.py
@@ -1,6 +1,5 @@
 from pylatex import Document, Section, Subsection, Command, NewLine, PageStyle, Package
 from pylatex.utils import italic, NoEscape, bold
-import pylatex.config
 
 class LatexGenerator(object):
 
@@ -21,27 +20,40 @@ class LatexGenerator(object):
         latex_document = Document(output_path)  
 
         # Import LaTeX packages
-        latex_document.packages.append(Package('titling'))   
+        """ USAGE:
+        latex_document.packages.append(Package('packagename', ['option1', 'option2', 'option3']))
+        """
+        latex_document.packages.append(Package('titling'))
+        latex_document.packages.append(Package('geometry', ['textwidth=350pt', 'textheight=600pt'])) # specify the dimensions of text area
 
         # Define and set variables
+        """ USAGE:
+        \command{value} -> latex_document.preamble.append(Command('command', 'value'))
+        \command{multiple}{values} -> latex_document.preamble.append(Command('command', ['multiple', 'values']))
+        if \subcommand appears among values -> NoEscape(r'\subcommand')
+        """
         latex_document.preamble.append(Command('title', 'The Drama'))
         latex_document.preamble.append(Command('author', 'Drama Generator 2000'))
         latex_document.preamble.append(Command('date', NoEscape(r'\today')))
+        latex_document.preamble.append(Command('fontsize', ['12pt', '20pt'])) # [font size, space between ines]
+        latex_document.preamble.append(Command('setlength', [NoEscape(r'\parindent'), '0pt'])) # remove indent at the begining of paragraphs
 
         # Create a title page
-        """
-        \begin{titlingpage}
-            \maketitle
-        \end{titlingpage}
-        """
         latex_document.append(NoEscape(r'\begin{titlingpage}'))
         latex_document.append(NoEscape(r'\maketitle'))
         latex_document.append(NoEscape(r'\end{titlingpage}'))
 
-        # Write drama
-        for message in self.messages:
-            latex_document.extend(self._generate_latex_for_message(message))
-            latex_document.append(NewLine())
+        # Write drama - currently commented out because smileys cause compilation errors
 
-        # Generate a pdf drama based on LaTeX file assembled above
-        latex_document.generate_pdf(clean_tex=True, compiler='xelatex')
+        # for message in self.messages:
+        #     latex_document.extend(self._generate_latex_for_message(message))
+        #     latex_document.append(NewLine())
+
+        # TODO: For encoding problems with smileys see https://www.overleaf.com/learn/how-to/What_file_encodings_and_line_endings_should_I_use%3F
+        
+        # Sample text to clearly see the text area, font size and space between the lines
+        latex_document.append('tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text tetx text')
+
+        # Generate a pdf drama based on LaTeX file assembled above. 
+        # Set clean_tex=True if you want .tex file deleted after it is compiled to pdf.
+        latex_document.generate_pdf(clean_tex=False, compiler='xelatex')
